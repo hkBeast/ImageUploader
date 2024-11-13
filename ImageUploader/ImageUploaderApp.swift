e906374108134cbf872a04cc1a331552cfa0ddbf
hkBeast <himanshu.singh@simplyentertaining.us>
2024-11-13 14:06:34 +0530

    Initial Commit

---
+++ ImageUploader/ImageUploaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageUploaderApp.swift
+//  ImageUploader
+//
+//  Created by HKBeast on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageUploaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
